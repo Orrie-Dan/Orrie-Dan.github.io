8c88c3f018d5b384bdee01de196f1899eb284a0d
Orrie-Dan <d.nkusi@alustudent.com>
2025-02-20 15:48:59 +0100

    Add files via upload
    Updated html file

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    if (menu) {
+        menu.classList.toggle('menu-active');
+    } else {
+        console.error("Menu not found! Check your class names.");
+    }
+}
